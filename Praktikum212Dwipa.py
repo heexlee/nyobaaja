abbab1fab1222d3d7fe0a9f8fdfd808bd5b61cc1
ran <84449848+heexlee@users.noreply.github.com>
2023-06-18 21:31:21 +0800

    Add files via upload

---
+++ Praktikum212Dwipa.py
@@ -0,0 +1,6 @@
+print('Program Menghitung')
+print('******************')
+angka1 = int(input("Ketik Angka Pertama : "))
+angka2 = int(input("Ketik Angka Kedua : "))
+hasil = angka1 +angka2
+print("Angka 1 + Angka 2 = ",hasil)
